d6e3a2f1cbaaf1e0594c9f2bb5505dec3e942150
Plagiator <pl4giator@gmail.com>
2014-02-20 21:12:00 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+if($.cookie("css")) {
+    $("#style").attr("href",$.cookie("css"));
+}
+$(document).ready(function() { 
+    $("a.color-box").click(function() { 
+         $("#style").attr("href",$(this).attr('rel'));		 
+         $.cookie("css",$(this).attr('rel'), {expires: 365, path: '/'});
+         return false;
+    });	 
+    $('.image-popup-vertical-fit').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        mainClass: 'mfp-img-mobile',
+        image: {
+            verticalFit: true
+        }
+    });
+});
